698104f0326ded1de6ee370c0aa5d9fc1127dbc4
Siebren Kranenburg <info@siebrenkranenburg.nl>
2024-05-16 15:00:57 +0200

    Setup nuspec for nuget pack & ship

--- src/Mollie.Api/Properties/AssemblyInfo.cs
+++ src/Mollie.Api/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mollie API Client")]
+[assembly: AssemblyTitle("Fonq.Mollie.Api")]
 [assembly: AssemblyDescription("fonQ's forked API Client")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("fonQ B.V.")]
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(true)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("dba9dc3a-d562-4d15-a7fb-b0a1dc3e517b")]
